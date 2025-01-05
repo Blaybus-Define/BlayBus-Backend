b292be2bd19c6a51838b50c649f3b1e7838f83b4
0sunset0 <chi6465618@naver.com>
2025-01-05 23:59:39 +0900

    first commit

---
+++ blaybus-backend/src/main/java/blaybus/blaybus_backend/BlaybusBackendApplication.java
@@ -0,0 +1,13 @@
+package blaybus.blaybus_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlaybusBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlaybusBackendApplication.class, args);
+	}
+
+}
---
+++ blaybus-backend/src/test/java/blaybus/blaybus_backend/BlaybusBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package blaybus.blaybus_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlaybusBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
