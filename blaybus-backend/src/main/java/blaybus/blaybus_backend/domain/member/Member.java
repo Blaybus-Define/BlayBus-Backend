00ef32994b2eda57a41a15951a9b487e5ccccd31
0sunset0 <chi6465618@naver.com>
2025-01-08 12:46:33 +0900

    feat: 도메인 초기 설계

---
+++ blaybus-backend/src/main/java/blaybus/blaybus_backend/domain/member/Member.java
@@ -0,0 +1,42 @@
+package blaybus.blaybus_backend.domain.member;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.time.LocalDate;
+
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id; // 기본 키 (자동 생성)
+
+    @Column(nullable = false, unique = true)
+    private String employeeNumber; // 사번
+
+    @Column(nullable = false)
+    private String name; // 이름
+
+    @Column(nullable = false)
+    private LocalDate hireDate; // 입사일
+
+    @Column(nullable = false)
+    private String department; // 소속
+
+    @Column(nullable = false)
+    private String jobGroup; // 직무그룹
+
+    @Column(nullable = false)
+    private MemberLevel memberLevel; // 레벨
+
+    @Column(nullable = false, unique = true)
+    private String username; // 아이디
+
+    @Column(nullable = false)
+    private String defaultPassword; // 기본 패스워드
+
+    private String updatedPassword; // 변경 패스워드
+
+    private int totalExperience; // 총 경험치
+}
---
+++ blaybus-backend/src/main/java/blaybus/blaybus_backend/domain/post/Post.java
@@ -0,0 +1,7 @@
+package blaybus.blaybus_backend.domain.post;
+
+public class Post {
+    private Long id;
+    private String title;
+    private String content;
+}
