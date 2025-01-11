a271cac84a80fa9db35ba2256aad6d0fffeac3ca
0sunset0 <chi6465618@naver.com>
2025-01-11 19:53:32 +0900

    docs: web config 변경

--- blaybus-backend/src/main/java/blaybus/blaybus_backend/global/config/WebConfig.java
+++ blaybus-backend/src/main/java/blaybus/blaybus_backend/global/config/WebConfig.java
@@ -28,7 +28,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 경로에 대해 CORS 설정
                 .allowedOrigins("http://localhost:3000", "http://192.168.123.159:3000") // 허용할 도메인
-                .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 메서드
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 메서드
                 .allowedHeaders("*") // 모든 헤더 허용
                 .allowCredentials(true) // 쿠키 허용
                 .maxAge(3600); // 캐싱 시간 (초)
