8239c6a55f0994ff6200f0bbaf1b36293a6b7331
0sunset0 <chi6465618@naver.com>
2025-01-16 14:54:47 +0900

    resolve conflicts

--- blaybus-backend/src/main/java/blaybus/blaybus_backend/domain/experience/service/ExperienceService.java
+++ blaybus-backend/src/main/java/blaybus/blaybus_backend/domain/experience/service/ExperienceService.java
@@ -1,28 +1,35 @@
 package blaybus.blaybus_backend.domain.experience.service;
 
+import blaybus.blaybus_backend.domain.admin.ExperienceQuestRequest;
+import blaybus.blaybus_backend.domain.admin.ExperienceQuestResponse;
 import blaybus.blaybus_backend.domain.experience.dto.ExpStatusResponseDTO;
 import blaybus.blaybus_backend.domain.experience.entity.ExperienceStatus;
+import blaybus.blaybus_backend.domain.experience.entity.GainExperience;
+import blaybus.blaybus_backend.domain.experience.repository.ExperienceRepository;
 import blaybus.blaybus_backend.domain.experience.repository.ExperienceStatusRepository;
-import blaybus.blaybus_backend.domain.member.dto.InfoResponseDTO;
 import blaybus.blaybus_backend.domain.member.entity.Member;
-import blaybus.blaybus_backend.global.common.SessionManager;
-import org.springframework.beans.factory.annotation.Autowired;
+import blaybus.blaybus_backend.domain.member.exception.MemberException;
+import blaybus.blaybus_backend.domain.member.repository.MemberRepository;
+import blaybus.blaybus_backend.domain.quest.entity.AchievementLevel;
+import blaybus.blaybus_backend.domain.quest.entity.MemberQuest;
+import blaybus.blaybus_backend.domain.quest.repository.MemberQuestRepository;
+import blaybus.blaybus_backend.global.exception.ErrorCode;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 @Transactional
+@RequiredArgsConstructor
 public class ExperienceService {
-    private SessionManager sessionManager;
-    public void findMyAllExperiences(Long memberId) {
-
-    }
+    private final MemberRepository memberRepository;
+    private final ExperienceRepository experienceRepository;
+    private final MemberQuestRepository memberQuestRepository;
     private final ExperienceStatusRepository expRepository;
-
-    @Autowired
-    public ExperienceService(ExperienceStatusRepository expRepository) {
-        this.expRepository = expRepository;
-    }
 
     // 총 경험치 조회
     public ExpStatusResponseDTO getExpStatusById(Long id) {
@@ -40,5 +47,37 @@ public class ExperienceService {
                 .projectExp(expStatus.getProjectExp())
                 .previousExp(expStatus.getPreviousExp())
                 .build();
+    }
+
+
+    public void grantExperience(ExperienceQuestRequest request) {
+        String loginId = request.getLoginId();
+        Member member = memberRepository.findByLoginId(loginId).orElseThrow(() -> new MemberException(ErrorCode.MEMBER_NOT_FOUND));
+
+        GainExperience gainExperience = GainExperience.builder()
+                .member(member)
+                .title(request.getTitle())
+                .type(request.getType())
+                .date(LocalDate.now())
+                .reason(request.getDescription())
+                .exp(request.getExperience())
+                .build();
+        experienceRepository.save(gainExperience);
+    }
+
+    public ExperienceQuestResponse findMyExperiences(String loginId) {
+        Member member = memberRepository.findByLoginId(loginId).orElseThrow(() -> new MemberException(ErrorCode.MEMBER_NOT_FOUND));
+        List<GainExperience> experiences = experienceRepository.findAllByMember(member);
+        return ExperienceQuestResponse.fromGainExperiences(experiences);
+    }
+
+    public ExperienceQuestResponse findNotAchievedOrFailedQuests(String loginId) {
+        Member member = memberRepository.findByLoginId(loginId).orElseThrow(() -> new MemberException(ErrorCode.MEMBER_NOT_FOUND));
+        List<MemberQuest> memberQuests = memberQuestRepository.findAllByMember(member);
+        List<MemberQuest> filteredQuests = memberQuests.stream()
+                .filter(quest -> quest.getAchievedLevel() == AchievementLevel.NOT_ACHIEVED ||
+                        quest.getAchievedLevel() == AchievementLevel.FAIL)
+                .collect(Collectors.toList());
+        return ExperienceQuestResponse.fromMemberQuests(filteredQuests);
     }
 }
