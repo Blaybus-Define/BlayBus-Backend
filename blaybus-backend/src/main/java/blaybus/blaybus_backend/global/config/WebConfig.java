e2e6d1bbfd157802214e91bfa7b20a28300830c8
0sunset0 <chi6465618@naver.com>
2025-01-17 05:18:06 +0900

    refactor: 허용 도메인 변경

--- blaybus-backend/src/main/java/blaybus/blaybus_backend/global/config/WebConfig.java
+++ blaybus-backend/src/main/java/blaybus/blaybus_backend/global/config/WebConfig.java
@@ -26,7 +26,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 경로에 대해 CORS 설정
-                .allowedOrigins("http://localhost:3000", "https://blaybus-define.github.io/") // 허용할 도메인
+                .allowedOrigins("http://localhost:3000", "https://blaybus-define.github.io") // 허용할 도메인
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 메서드
                 .allowedHeaders("*") // 모든 헤더 허용
                 .allowCredentials(true) // 쿠키 허용
